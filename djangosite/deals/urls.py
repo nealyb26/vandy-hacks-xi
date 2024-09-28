a87d6e430b7e6278dfaca8e8908d9c2d2d153c71
Simon Kassman <sjkass@att.net>
2024-09-28 15:31:30 -0500

    small template changes and adding more example posts

--- djangosite/deals/urls.py
+++ djangosite/deals/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "deals"
 urlpatterns = [
     path("", views.home, name="home"),
     path("post/<int:post_id>/", views.detail, name="detail"),
