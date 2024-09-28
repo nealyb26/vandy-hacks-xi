1dc6202bc4b546cd86067e74859c5f9bb54cd42c
Simon Kassman <sjkass@att.net>
2024-09-28 14:51:56 -0500

    django: basic views for displaying posts and comments

--- djangosite/deals/views.py
+++ djangosite/deals/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("This is the homepage for the deals app!")
+from .models import Post, Comment
+
+def home(request) -> HttpResponse:
+    context = {"post_list": Post.objects.order_by("-post_date")}
+    return render(request, "deals/home.html", context)
+
+def detail(request, post_id: int) -> HttpResponse:
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, "deals/detail.html", {"post": post})
