c0fa79c419334cb49329a1917ea9b64e6a01aa42
Simon Kassman <sjkass@att.net>
2024-09-28 14:29:07 -0500

    django: created post and comment models

--- djangosite/deals/models.py
+++ djangosite/deals/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    product_name = models.CharField(max_length=200)
+    location = models.CharField(max_length = 200) # TODO: Change to location info
+    info_text = models.CharField(max_length = 200)
+    score = models.IntegerField(default=0)
+    post_date = models.DateTimeField("date posted")
+
+    def __str__(self) -> str:
+        return f"{self.product_name}"
+    
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    body_text = models.CharField(max_length=1000)
+    post_date = models.DateTimeField("date posted")
+
+    def __str__(self) -> str:
+        return f"Post: {self.post}; Text: \"{self.body_text:.20}\""
