2e8363b4910ecea29758182a3c7089ca41462d97
Neal Bagai <45185138+nealyb26@users.noreply.github.com>
2024-09-29 01:19:55 -0500

    commit - location cookies hopefully working

--- djangosite/deals/views.py
+++ djangosite/deals/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.utils import timezone
 from django.urls import reverse
+from geopy.distance import geodesic
 from django.db.models import Q, F
 
 from .models import Post, Comment
@@ -50,8 +51,46 @@ def search(request) -> HttpResponse:
     if "sort" in request.GET:
         sort = request.GET["sort"]
         # e.g. '-score'
-    
+
+    user_lat = request.COOKIES.get('user_lat')
+    user_long = request.COOKIES.get('user_long')
+    print(user_lat, user_long)
+
+    if user_lat and user_long:
+        user_coordinates = (float(user_lat), float(user_long))
+    else:
+        user_coordinates =  None
+
     post_list = Post.objects.filter(filter_set).order_by(sort)
+    post_data = []
+
+    if user_coordinates:
+        for post in post_list:
+            post_coordinates = (post.location_lat, post.location_long)
+            distance = geodesic(user_coordinates, post_coordinates).miles
+            post_data.append({
+                'post': post,
+                'distance': round(distance, 2),  # Round to 2 decimal places
+            })
+    else:
+        post_data = [{'post': post, 'distance': None} for post in post_list]
+    context = {
+        'query': get_query,
+        'post_data': post_data
+    }
+
+    #context = {"post_list": Post.objects.order_by("-post_date")}
+    response = render(request, 'deals/search.html', context)
+
+    if not user_coordinates:
+        # Use JavaScript to get user location and set the cookies (explained below)
+        response.set_cookie('user_lat', '')
+        response.set_cookie('user_long', '')
+
+    return response
+    #return render(request, "deals/home.html", context)
+    
+    
 
 
     return render(request, "deals/search.html", {"query": get_query, "post_list" : post_list})
